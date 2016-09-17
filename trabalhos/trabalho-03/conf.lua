bcd1f9961c76b6031f9ec1dc42593ebfaed56987
Luca Ribeiro Pereira Carneiro <lucaribeiroxd@hotmail.com>
2016-09-16 23:12:49 -0300

    Trabalho 03

---
+++ trabalhos/trabalho-03/conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+	t.title = "Touhou Game (easy mode)" 
+	t.version = "0.10.1"        
+	t.window.width = 480        
+	t.window.height = 960
+
+	-- Para consertar bugs no windows
+	t.console = true
+end
